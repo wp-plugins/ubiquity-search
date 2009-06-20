8123f70b81500a5236f41ab3d0cebb4fe1bca89b
pfefferle <pfefferle@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-20 09:30:19 +0000

    changes for 2.8
    git-svn-id: https://plugins.svn.wordpress.org/ubiquity-search/trunk@127678 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ubiquity-search.php
+++ ubiquity-search.php
@@ -8,16 +8,6 @@ Author: Matthias Pfefferle
 Author URI: http://notizblog.org/
 */
 
-// Pre-2.6 compatibility
-if ( ! defined( 'WP_CONTENT_URL' ) )
-    define( 'WP_CONTENT_URL', get_option( 'siteurl' ) . '/wp-content' );
-if ( ! defined( 'WP_CONTENT_DIR' ) )
-    define( 'WP_CONTENT_DIR', ABSPATH . 'wp-content' );
-if ( ! defined( 'WP_PLUGIN_URL' ) )
-    define( 'WP_PLUGIN_URL', WP_CONTENT_URL. '/plugins' );
-if ( ! defined( 'WP_PLUGIN_DIR' ) )
-    define( 'WP_PLUGIN_DIR', WP_CONTENT_DIR . '/plugins' );
-
 // register
 if (isset($wp_version)) {
   add_filter('query_vars', array('UbiquitySearch', 'queryVars'));
